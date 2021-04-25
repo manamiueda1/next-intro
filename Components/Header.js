6629b162807635cde4d43a1cca89e155e1f9d2b3
Manami Ueda <manamiueda1@gmail.com>
2021-04-25 16:20:12 -0400

    added header

---
+++ Components/Header.js
@@ -0,0 +1,17 @@
+import headerStyles from '../styles/Header.module.css'
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className={headerStyles.title}>
+                <span>WebDev</span> News
+            </h1>
+           <p classNAme={headerStyles.description}>
+               Keep up to date with the latest Webdev News
+           </p>
+        </div>
+    )
+}
+
+export default Header
